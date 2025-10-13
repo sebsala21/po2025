1f71470522bdc71dbe0360b3c692181d5b39df7c
sebek <sebek.salach@wp.pl>
2025-10-13 19:27:34 +0200

    sebastian salach lab1

--- Lab1/Hello.java
+++ Lab1/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
-public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
 }
